8f845a55cb6757698598ca4ca94385998916e84f
vishal.Alok <you@example.com>
2021-10-18 05:03:50 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+namespace Examples
+{
+
+    class Geeks
+    {
+        public static int Add(params int[] ListNumbers)
+        {
+            int total = 0;
+            foreach (int i in ListNumbers)
+            {
+                total += i;
+            }
+            return total;
+        } 
+        static void Main(string[] args)
+        {
+            int y = Add(12, 13, 10, 15, 56);
+            Console.WriteLine(y);
+        }
+    }
+}
